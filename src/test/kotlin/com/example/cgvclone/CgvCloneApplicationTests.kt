7eaa3d29657ae3b4ba7c2e38141cf10729cb6102
jin-ha <jin-ha@nhn-commerce.com>
2023-11-14 21:13:28 +0900

    Initial commit

---
+++ src/test/kotlin/com/example/cgvclone/CgvCloneApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.cgvclone
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CgvCloneApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
